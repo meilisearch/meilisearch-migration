32e13db3e6c5ad215758a2eba0502a3fbcad9d94
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-09-23 15:56:55 +0200

    Add script and usage

---
+++ README2.md
@@ -0,0 +1,158 @@
+<p align="center">
+  <img src="https://github.com/meilisearch/integration-guides/blob/main/assets/logos/logo.svg" alt="MeiliSearch Cloud scripts" width="200" height="200" />
+</p>
+
+<h1 align="center">MeiliSearch Version Migration Script</h1>
+
+<h4 align="center">
+  <a href="https://github.com/meilisearch/MeiliSearch">MeiliSearch</a> |
+  <a href="https://docs.meilisearch.com">Documentation</a> |
+  <a href="https://slack.meilisearch.com">Slack</a> |
+  <a href="https://roadmap.meilisearch.com/tabs/1-under-consideration">Roadmap</a> |
+  <a href="https://www.meilisearch.com">Website</a> |
+  <a href="https://docs.meilisearch.com/faq">FAQ</a>
+</h4>
+
+<p align="center">
+  <a href="https://github.com/meilisearch/meilisearch-migration/blob/main/LICENSE"><img src="https://img.shields.io/badge/license-MIT-informational" alt="License"></a>
+</p>
+
+<p align="center">MeiliSearch tools for the Cloud</p>
+
+**MeiliSearch Version Update Script** is a script that migrates your MeiliSearch from a version to another more recent version, it also works with release candidates.
+
+**MeiliSearch** is an open-source search engine. [Discover what MeiliSearch is!](https://github.com/meilisearch/MeiliSearch)
+
+## Table of Contents <!-- omit in toc -->
+
+- [Requirements](#-Requirements)
+- [Usage](#-Usage
+- [Features](#-features)
+- [📖 Documentation](#-documentation)
+- [⚙️ Development Workflow and Contributing](#️-development-workflow-and-contributing)
+
+## Requirements
+
+### 1. Systemctl
+
+Currently the script only works in an environment in which MeiliSearch is running as a `systemctl` service.
+
+### 2. Correct data.ms path
+
+The MeiliSearch's `data.ms` must be stored at the following address: `/var/lib/meilisearch/data.ms`.<br>
+To ensure this is the case, MeiliSearch should have started with the following flags:`--db-path /var/lib/meilisearch/data.ms`
+
+You can check the information by looking at the file located here `cat /etc/systemd/system/meilisearch.service`.<br>
+You should find a line with the specific command used.
+
+```bash
+ExecStart=/usr/bin/meilisearch --db-path /var/lib/meilisearch/data.ms --env production
+```
+
+By default all MeiliSearch instances created using one of our cloud providers are storing `data.ms` in this repository.
+
+### 3. A Running MeiliSearch instance
+
+A MeiliSearch instance should be running before launching the script. This can be checked using the following command:
+
+```bash
+systemctl status meilisearch
+```
+
+If you don't have a running MeiliSearch instance, you can create one using one of our Cloud Providers:
+
+| Cloud Provider | Project                                                                              |
+| -------------- | :----------------------------------------------------------------------------------- |
+| DigitalOcean   | [meilisearch-digitalocean](https://github.com/meilisearch/meilisearch-digitalocean/) |
+| AWS            | [meilisearch-aws](https://github.com/meilisearch/meilisearch-aws/)                   |
+| GCP            | [meilisearch-gcp](https://github.com/meilisearch/meilisearch-gcp/)                   |
+
+<br>
+
+Alternatively, by [downloading and running MeiliSearch](https://docs.meilisearch.com/learn/getting_started/installation.html#download-and-launch) on your own server and start it as a [systemctl service](https://www.freedesktop.org/software/systemd/man/systemctl.html).
+
+## Usage
+
+⚠️ In case of failure during the execution of the script and for security purposes, we strongly recommend [creating manually your own dump](https://docs.meilisearch.com/reference/features/dumps.html#creating-a-dump).
+
+To launch the script you should open the server using SSH and run the following command:
+
+```bash
+sh update_meilisearch_version meilisearch_version
+```
+
+- `meilisearch_version`: the MeiliSearch version formatted like this: `vX.X.X`
+
+### Example:
+
+An official release:
+
+```bash
+sh update_meilisearch_version.sh v0.22.0
+```
+
+A release candidate:
+
+```bash
+sh update_meilisearch_version.sh v0.22.0rc1
+```
+
+## Features
+
+- [Automatic Dumps](#automatic-dumps) export and import in case of version incompatibility.
+- [Rollback](Rollback in case of failure) in case of failure.
+
+### Automatic Dumps
+
+The script is made to migrate the data properly in case the required version is not compatible with the current version.
+
+It is done by doing the following:
+
+- Create a dump
+- Stop MeiliSearch service
+- Download and update MeiliSearch
+- Start MeiliSearch
+- If the start fails because versions are not compatible:
+  - Delete current `data.ms`
+  - Import the previously created dump
+  - Restart MeiliSearch
+- Remove generated dump file.
+
+### Rollback in case of failure
+
+If something goes wrong during the version update process a rollback occurs:
+
+- The script rolls back to the previous MeiliSearch version by using the previous cached MeiliSearch binary.
+- The previous `data.ms` is used and replaces the new one to ensure MeiliSearch works exactly as before the script was used.
+- MeiliSearch is started again.
+
+Example:
+Your current version is `v0.21.0` you want to update MeiliSearch to `v0.22.0`. Thus inside your server you import and launch the script
+
+```
+sh update_meilisearch_version.sh v0.22.0
+```
+
+The migration fails for whatever reason. The script uses the cached `v0.21.0` binary and the cached `data.ms` of the previous version to rollback to its original state.
+
+## Version incompatibilities
+
+If your settings are not compatible between versions, you will have to re-index your data as importing the dump will fail.
+
+## Gif example
+
+![](../../assets/version_update.gif)
+
+## 📖 Documentation
+
+See our [Documentation](https://docs.meilisearch.com/learn/tutorials/getting_started.html) or our [API References](https://docs.meilisearch.com/reference/api/).
+
+## ⚙️ Development Workflow and Contributing
+
+Any new contribution is more than welcome in this project!
+
+If you want to know more about the development workflow or want to contribute, please visit our [contributing guidelines](/CONTRIBUTING.md) for detailed instructions!
+
+<hr>
+
+**MeiliSearch** provides and maintains many **SDKs and Integration tools** like this one. We want to provide everyone with an **amazing search experience for any kind of project**. If you want to contribute, make suggestions, or just know what's going on right now, visit us in the [integration-guides](https://github.com/meilisearch/integration-guides) repository.
---
+++ scripts/update_meilisearch_version_2.sh
@@ -0,0 +1,278 @@
+#!/bin/bash
+
+## Constants
+
+RED="\033[0;31m"
+BRED="\033[1;31m"
+BGREEN="\033[1;32m"
+BYELLOW="\033[1;33m"
+BBLUE="\033[1;34m"
+BPINK="\033[1;35m"
+WHITE="\033[0m"
+NC="\033[0m"
+ERROR_LABEL="${BRED}error: ${NC}"
+SUCCESS_LABEL="${BGREEN}success: ${NC}"
+PENDING_LABEL="${BYELLOW}pending: ${NC}"
+INFO_LABEL="${BBLUE}info: ${NC}"
+
+## Utils Functions
+
+# Rollback to previous MeiliSearch version in case something went wrong
+
+previous_version_rollback() {
+
+    echo "${ERROR_LABEL}MeiliSearch update to $meilisearch_version failed." >&2
+    echo "${INFO_LABEL}Rollbacking to previous version ${BPINK}$current_meilisearch_version${NC}." >&2
+    echo "${INFO_LABEL}Recovering..." >&2
+    mv /tmp/meilisearch /usr/bin/meilisearch
+    echo "${SUCCESS_LABEL}Recover previous data.ms." >&2
+    mv /tmp/data.ms /var/lib/meilisearch/data.ms
+    echo "${INFO_LABEL}Restarting MeiliSearch." >&2
+    systemctl restart meilisearch
+    systemctl_status exit
+    echo "${SUCCESS_LABEL}Previous MeiliSearch version ${BPINK}$current_meilisearch_version${NC} restarted correctly with its data recovered." >&2
+    delete_temporary_files
+    echo "${ERROR_LABEL}Update MeiliSearch from ${BPINK}$current_meilisearch_version${NC} to ${BPINK}$meilisearch_version${NC} failed. Rollback to previous version successfull." >&2
+    exit
+}
+
+# Check if MeiliSearch systemctl process is Active
+systemctl_status() {
+    systemctl status meilisearch | grep -E 'Active: active \(running\)' -q
+    grep_status_code=$?
+    callback_1=$1
+    callback_2=$2
+    if [ $grep_status_code -ne 0 ]; then
+        echo "${ERROR_LABEL}MeiliSearch Service is not Running. Please start MeiliSearch." >&2
+        if [ ! -z "$callback_1" ]; then
+            $callback_1
+        fi
+
+        if [ ! -z "$callback_2" ]; then
+            2
+            $callback_2
+        fi
+
+    fi
+}
+
+# Delete temporary files
+delete_temporary_files() {
+    echo "${INFO_LABEL}Cleaning temporary files..."
+    if [ -f "meilisearch" ]; then
+        rm meilisearch
+        echo "${SUCCESS_LABEL}Delete temporary meilisearch binary."
+    fi
+
+    if [ -f "logs" ]; then
+        rm logs
+        echo "${SUCCESS_LABEL}Delete temporary logs file."
+    fi
+
+    local dump_file="/dumps/$dump_id.dump"
+    if [ -f $dump_file ]; then
+        rm "$dump_file"
+        echo "${SUCCESS_LABEL}Delete temporary dump file."
+    fi
+
+}
+
+# Check if MeiliSearch arguments are provided
+check_args() {
+    if [ $1 -eq 0 ]; then
+        echo "${ERROR_LABEL}$1"
+        exit
+    fi
+}
+
+# Check if latest command exit status is not an error
+check_last_exit_status() {
+    status=$1
+    message=$2
+    callback_1=$3
+    callback_2=$4
+
+    if [ $status -ne 0 ]; then
+        echo "${ERROR_LABEL}$messag."
+        if [ ! -z "$callback_1" ]; then
+            ($callback_1)
+        fi
+        if [ ! -z "$callback_2" ]; then
+            ($callback_2)
+        fi
+        exit
+    fi
+}
+
+## Main Script
+
+#
+# Current Running MeiliSearch
+#
+
+echo "${SUCCESS_LABEL}Starting version update of MeiliSearch."
+
+# Check if MeiliSearch Service is running
+systemctl_status exit
+
+# Check if version argument was provided on script launch
+check_args $# "MeiliSearch Version not provided as arg.\nUsage: sh update_meilisearch_version.sh [vX.X.X]"
+
+# Version to update MeiliSearch to.
+meilisearch_version=$1
+
+echo "${SUCCESS_LABEL}Requested MeiliSearch version: ${BPINK}$meilisearch_version${NC}."
+
+# Current MeiliSearch version
+current_meilisearch_version=$(
+    curl -X GET 'http://localhost:7700/version' --header "X-Meili-API-Key: $MEILISEARCH_MASTER_KEY" -s --show-error |
+        cut -d '"' -f 12
+)
+
+# Check if curl version request is successfull.
+check_last_exit_status $? "Version request 'GET /version' request failed."
+echo "${SUCCESS_LABEL}Current running MeiliSearch version: ${BPINK}$current_meilisearch_version${NC}."
+
+#
+# Back Up Dump
+#
+
+# Create dump for migration in case of incompatible versions
+echo "${INFO_LABEL}Creation of a dump in case new version does not have compatibility with the current MeiliSearch."
+dump_return=$(curl -X POST 'http://localhost:7700/dumps' --header "X-Meili-API-Key: $MEILISEARCH_MASTER_KEY" --show-error -s)
+
+# Check if curl request was successfull.
+check_last_exit_status $? "Dump creation 'POST /dumps' request failed."
+
+# Get the dump id
+dump_id=$(echo $dump_return | cut -d '"' -f 4)
+echo "${INFO_LABEL}Creating dump id with id: $dump_id."
+
+# Check if curl call succeeded to avoid infinite loop. In case of fail exit and clean
+response=$(curl -X GET "http://localhost:7700/dumps/$dump_id/status" --header "X-Meili-API-Key: $MEILISEARCH_MASTER_KEY" --show-error -s)
+if echo response | grep '"status":"failed"' -q; then
+    echo "${ERROR_LABEL}MeiliSearch could not create the dump:\n ${response}" >&2
+    delete_temporary_files
+    exit
+fi
+
+check_last_exit_status $? \
+    "Dump status check 'POST /dumps/:dump_id/status' request failed." \
+    delete_temporary_files dump_id
+
+# Wait for Dump to be created
+until curl -X GET "http://localhost:7700/dumps/$dump_id/status" \
+    --header "X-Meili-API-Key: $MEILISEARCH_MASTER_KEY" --show-error -s |
+    grep '"status":"done"' -q; do
+    echo "${PENDING_LABEL}MeiliSearch is still creating the dump: $dump_id."
+    sleep 2
+done
+
+echo "${SUCCESS_LABEL}MeiliSearch finished creating the dump: $dump_id."
+
+#
+# New MeiliSsarch
+#
+
+# Download MeiliSearch of the right version
+echo "${INFO_LABEL}Downloading MeiliSearch version ${BPINK}$meilisearch_version${NC}."
+response=$(curl "https://github.com/meilisearch/MeiliSearch/releases/download/$meilisearch_version/meilisearch-linux-amd64" --output meilisearch --location -s --show-error)
+
+check_last_exit_status $? \
+    "Request to download MeiliSearch $meilisearch_version release failed." \
+    delete_temporary_files
+
+# Give read and write access to meilisearch binary
+chmod +x meilisearch
+
+# Check if MeiliSearch binary is not corrupted
+if file meilisearch | grep "ELF 64-bit LSB shared object" -q; then
+    echo "${SUCCESS_LABEL}Successfully downloaded MeiliSearch version $meilisearch_version."
+else
+    echo "${ERROR_LABEL}MeiliSearch binary is corrupted.\n\
+  It may be due to: \n\
+  - Invalid version syntax. Provided: $meilisearch_version, expected: vX.X.X. ex: v0.22.0 \n\
+  - Rate limiting from GitHub." >&2
+    delete_temporary_files
+    exit
+fi
+
+echo "${INFO_LABEL}Stopping MeiliSearch Service to update the version."
+## Stop meilisearch running
+systemctl stop meilisearch # stop le service pour pouvoir changer la version
+
+## Move le nouveau binaire
+echo "${INFO_LABEL}Keep a temporary copy of previous MeiliSearch."
+mv /usr/bin/meilisearch /tmp
+
+## Copy
+echo "${INFO_LABEL}Update MeiliSearch version."
+cp meilisearch /usr/bin/meilisearch
+
+## Restart MeiliSearch
+systemctl restart meilisearch
+echo "${INFO_LABEL}MeiliSearch $meilisearch_version is starting."
+
+if systemctl status meilisearch | grep -E 'Active: active \(running\)' -q; then
+    echo "${INFO_LABEL}Both MeiliSearch versions are compatible. No need to dump."
+else
+    echo "${INFO_LABEL}MeiliSearch versions are not compatible with each other."
+
+    # Stopping MeiliSearch Service
+    systemctl stop meilisearch
+    echo "${INFO_LABEL}Stop MeiliSearch $meilisearch_version service."
+
+    # Keep cache of previous data.ms in case of failure
+    cp -r /var/lib/meilisearch/data.ms /tmp/
+    echo "${INFO_LABEL}Copy data.ms to be able to recover in case of failure."
+
+    # Remove data.ms
+    rm -rf /var/lib/meilisearch/data.ms
+    echo "${INFO_LABEL}Delete current MeiliSearch's data.m."
+
+    # Run MeiliSearch
+    MEILI_IMPORT_DUMP="/dumps/$dump_id.dump"
+    ./meilisearch --db-path /var/lib/meilisearch/data.ms --env production --import-dump "/dumps/$dump_id.dump" --master-key $MEILISEARCH_MASTER_KEY 2>logs &
+    echo "${INFO_LABEL}Run local $meilisearch_version binary importing the dump and creating the new data.ms."
+
+    sleep 2
+
+    # Needed conditions due to bug in MeiliSearch #1701
+    if cat logs | grep "Error: No such file or directory (os error 2)" -q; then
+        # If dump was empty no import is needed
+        echo "${SUCCESS_LABEL}Empty database! Importing of no data done."
+    else
+        echo "${INFO_LABEL}Check if local $meilisearch_version started correctly."
+        # Check if local meilisearch started correctly `./meilisearch ..`
+        if ps | grep "meilisearch" -q; then
+            echo "${SUCCESS_LABEL}MeiliSearch started successfully and is importing the dump."
+        else
+            echo "${ERROR_LABEL}MeiliSearch could not start: \n ${BRED}$(cat logs)${NC}." >&2
+            # In case of failed start rollback to initial version
+            previous_version_rollback
+        fi
+
+        ## Wait for pending dump indexation
+        until curl -X GET 'http://localhost:7700/health' -s >/dev/null; do
+            echo "${PENDING_LABEL}MeiliSearch is still indexing the dump."
+            sleep 2
+        done
+
+        echo "${SUCCESS_LABEL}MeiliSearch is done indexing the dump."
+        # Kill local MeiliSearch process
+        pkill meilisearch
+        echo "${INFO_LABEL}Kill local MeiliSearch process."
+
+        # Restart MeiliSearch
+        systemctl restart meilisearch
+        echo "${INFO_LABEL}MeiliSearch $meilisearch_version service is starting."
+        # In case of failed restart rollback to initial version
+        systemctl_status previous_version_rollback exit
+        echo "${SUCCESS_LABEL}MeiliSearch $meilisearch_version service started succesfully."
+    fi
+fi
+
+# Delete temporary files to leave the environment the way it was initially
+delete_temporary_files
+
+echo "${BGREEN}Migration complete. MeiliSearch is now in version ${NC} ${BPINK}$meilisearch_version${NC}."
